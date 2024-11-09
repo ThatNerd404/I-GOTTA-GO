a57b6b20bae3d7913b0310b2fad8784ab393b65f
ThatNerd404 <106496451+ThatNerd404@users.noreply.github.com>
2024-11-09 15:31:11 -0600

    ughadsfgadf

--- Main.py
+++ Main.py
@@ -10,8 +10,8 @@ def main():
     Window_Width, Window_Height = 1280, 720
     pygame.display.set_caption("Summer In December!!!")
     pygame.display.set_icon(snowman_icon)
-    display_surface = pygame.display.set_mode((Window_Width, Window_Height))
-    display_surface.fill('royal blue1') 
+    screen = pygame.display.set_mode((Window_Width, Window_Height))
+    screen.fill('royal blue1') 
     
     #? setting time for the framerate
     clock = pygame.time.Clock()
@@ -27,19 +27,20 @@ def main():
                 Game_Running = False   
         
         #? wipes away last frame
-        display_surface.fill('royal blue1')
+        screen.fill('royal blue1')
         
-        #? draw player on screen
-        snowman_player = pygame.image.load("Assets\Img\THE_SNOWMAN.png")
+        #? load player and resize
+        snowman_player = pygame.image.load("Assets\Img\THE_SNOWMAN.png").convert_alpha() #* apparently adding convert_alpha makes the framerate go up ¯\_(ツ)_/¯
         snowman_player = pygame.transform.scale(snowman_player, (128,128))
-        display_surface.blit(snowman_player, player_pos)
         
+        #? place player on screen and controls
+        screen.blit(snowman_player, player_pos)
         keys = pygame.key.get_pressed()
-        if keys[pygame.K_w] or keys[pygame.K_UP]:
+        if keys[pygame.K_w] or keys[pygame.K_UP] or keys[pygame.K_SPACE]: #* Decreasing y moves you up and Increasing y moves you down 
             player_pos.y -= 600 * dt
         if keys[pygame.K_s] or keys[pygame.K_DOWN]:
             player_pos.y += 600 * dt
-        if keys[pygame.K_a] or keys[pygame.K_LEFT]:
+        if keys[pygame.K_a] or keys[pygame.K_LEFT]: #* Decreasing x moves you left and Increasing x moves you right
             player_pos.x -= 600 * dt
         if keys[pygame.K_d] or keys[pygame.K_RIGHT]:
             player_pos.x += 600 * dt
@@ -48,12 +49,12 @@ def main():
         pygame.display.update() #* or .flip as flip does only a part of the display while .update does the entire display
         
         #? limits the frame rate to 60
-        
         dt = clock.tick(60) / 1000
         
 
     #? closes game properly
     pygame.quit()
     sys.exit()
+    
 if __name__ == "__main__":
     main()
