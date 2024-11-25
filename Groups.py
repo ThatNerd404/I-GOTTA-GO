7a4a57322b77cbdae3ae84284d6a749d2c384541
ThatNerd404 <106496451+ThatNerd404@users.noreply.github.com>
2024-11-25 05:10:24 -0600

    tiny change to offset value

--- Groups.py
+++ Groups.py
@@ -6,7 +6,7 @@ class AllSprites(pygame.sprite.Group):
         self.screen = pygame.display.get_surface()
         self.offset = pygame.Vector2()
     def draw(self, surface, target_pos):
-        self.offset.x = -(target_pos[0] - Window_Width / 2)
-        self.offset.y = -(target_pos[1] - Window_Height / 2)
+        self.offset.x = target_pos[0] - Window_Width / 2
+        self.offset.y = target_pos[1] - Window_Height / 2
         for sprite in self:
-            surface.blit(sprite.image, sprite.rect.topleft + self.offset)
+            surface.blit(sprite.image, sprite.rect.topleft - self.offset)
